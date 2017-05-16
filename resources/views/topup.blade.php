dcc48aefe98765e887f80add7555b218f1c85802
Unknown <kaniaamalia21@gmail.com>
2017-05-16 14:14:23 +0700

    topup page

--- resources/views/topup.blade.php
+++ resources/views/topup.blade.php
@@ -4,11 +4,12 @@
 <head>
 
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
 
     <title>BUNGKUS.IN</title>
+    <link rel="shortcut icon" href="{{url('temp/nasi.png')}}">
 
     <!-- Bootstrap core CSS -->
     <link href="temp/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
@@ -35,7 +36,7 @@
 <body>
 
     <!-- Navigation -->
-    <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse" style="background-color: #ffce99">
+    <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -47,14 +48,14 @@
 
                  <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="{{url('/history')}}">Pesanan</a>
+                        <a class="nav-link" href="{{url('/history')}}">History Pesanan</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Rp 28.000</a>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="{{url('/topup')}}">Rp 28.000</a>
                     </li>
                    <!--  <li class="nav-item">
                         <a class="nav-link" href="#">Contact</a>
-                    </li> --> -->
+                    </li> --> 
                 </ul>
             </div>
         </div>
@@ -63,56 +64,94 @@
 
     <div class="container">
     <div class="span12">
-    <div class="">
-            <style>
-        h5 {
-            color: #3CB371;
-        } </style>
-        <hr class="soften"/>    
+    <div class="">    
         <div class="table-responsive">
             <table class="table table-bordered table-condensed ">
                 
                 <tbody>
                     <tr>
-                        <td colspan="2"><center>BungPay : <b>Rp 29.000</b></center></td>
+                        <td colspan="2" style="background-color:    #f0ad4e"><center>BungPay : <b><h1>Rp 29.000</h1></b></center></td>
                     </tr>
                     <tr>
-                        <td colspan="1"><a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample"><center>By Transfer</center></a>
+                        <td align="center">
+                            <a style="width: 100%" role="button" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                            <img src="{{url('temp/transfer.png')}}" style="width: 50px;height: 50px"><br>
+                            <center><b>Via Transfer Bank</b></center></a>
                             <div class="collapse" id="collapseExample">
                               <div class="well">
                               <br>
-                                Pilih Nominal<br>
+                                <label>Pilih Nominal</label><br>
                                 <form class="form-group" method="get" action="{{url('transfer')}}">
-                                  <input type="radio" name="gender" value="10000" checked> Rp 10.000<br>
-                                  <input type="radio" name="gender" value="25000"> Rp 25.000<br>
-                                  <input type="radio" name="gender" value="50000"> Rp 50.000<br><br>
-                                  <input type="submit" value="submit">
-                                </form>                                
+                                  <label class="radio-inline"><input type="radio" name="gender" value="10000" checked> Rp 10.000</label><br>
+                                  <label class="radio-inline"><input type="radio" name="gender" value="25000"> Rp 25.000</label><br>
+                                  <label class="radio-inline"><input type="radio" name="gender" value="50000"> Rp 50.000</label><br><br>
+                                  <input class="btn btn-primary" type="submit" value="Pilih">
+                                </form>                             
                               </div>
                             </div>
                         </td>
-
-                        <td colspan="1"><a class="btn btn-primary" role="button" data-toggle="collapse" href="#collapseExample1" aria-expanded="false" aria-controls="collapseExample"><center>By Pulsa</center></a>
+                    </tr>
+                    <tr>
+                        <td align="center">
+                            <a style="width: 100%" role="button" data-toggle="collapse" href="#collapseExample1" aria-expanded="false" aria-controls="collapseExample1">
+                            <img src="{{url('temp/hp.png')}}" style="width: 50px;height: 50px"><br>
+                            <center><b>Via Pulsa</b></center></a>
                             <div class="collapse" id="collapseExample1">
                               <div class="well">
-                                <br>
-                                Pilih Nominal<br>
+                              <br>
+                                <label>Pilih Nominal</label><br>
                                 <form class="form-group" method="get" action="{{url('pulsa')}}">
-                                  <input type="radio" name="gender" value="10000" checked> Rp 10.000<br>
-                                  <input type="radio" name="gender" value="25000"> Rp 25.000<br>
-                                  <input type="radio" name="gender" value="50000"> Rp 50.000<br><br>
-                                  <input type="submit" value="submit">
-                                </form>  
+                                  <label class="radio-inline"><input type="radio" name="gender" value="10000" checked> Rp 10.000</label><br>
+                                  <label class="radio-inline"><input type="radio" name="gender" value="25000"> Rp 25.000</label><br>
+                                  <label class="radio-inline"><input type="radio" name="gender" value="50000"> Rp 50.000</label><br><br>
+                                  <input class="btn btn-primary" type="submit" value="Pilih">
+                                </form>                             
                               </div>
                             </div>
                         </td>
-                       
+                    </tr>
+                    <tr>
+                        <td align="center">                              
+                            <a style="width: 100%" role="button" data-toggle="collapse" href="#collapseExample2" aria-expanded="false" aria-controls="collapseExample2">
+                            <img src="{{url('temp/topup.png')}}" style="width: 50px;height: 50px"><br>
+                            <center><b>History BungPay</b></center></a>
+                            <div class="collapse" id="collapseExample2">
+                              <div class="well">
+                                  <br>
+                                    <div style="overflow-x:auto;">
+                                      <table class="table table-hover">
+                                          <thead>
+                                            <th>Tanggal</th>
+                                            <th>Topup/Pakai</th>
+                                            <th>Jumlah</th>
+                                          </thead>
+                                          <tr>
+                                            <td>15 April 2017</td>
+                                            <td><p style="color:#5cb85c">Topup</p></td>
+                                            <td>50.000</td>
+                                          </tr>
+                                          <tr>
+                                            <td>13 April 2017</td>
+                                            <td><p style="color:#d9534f">Pakai</p></td>
+                                            <td>20.000</td>
+                                          </tr>
+                                          <tr>
+                                            <td>1 April 2017</td>
+                                            <td><p style="color:#5cb85c">Topup</p></td>
+                                            <td>50.000</td>
+                                          </tr>
+                                      </table>
+                                    </div> 
+                              </div>
+                            </div>
+                        </td>
+                    </tr>
 
+                       
 
   
 
-                    </tr>
-
+                 
 
 
                    
